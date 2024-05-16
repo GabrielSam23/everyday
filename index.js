ae3f2c62832c8afa6438b39030f95ad556be11dd
GabrielSam23 <143095210+GabrielSam23@users.noreply.github.com>
2024-05-16 00:02:17 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -3,12 +3,18 @@ const http = require('http');
 const socketIo = require('socket.io');
 const bodyParser = require('body-parser');
 const { Pool } = require('pg');
-require('dotenv').config(); // Para carregar as variáveis de ambiente do arquivo .env
+require('dotenv').config();
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
-const pool = new Pool(); // Usará as variáveis de ambiente para configuração
+const pool = new Pool({
+    user: process.env.PGUSER,
+    host: process.env.PGHOST,
+    database: process.env.PGDATABASE,
+    password: process.env.PGPASSWORD,
+    port: 5432,
+});
 
 app.use(express.static('.'));
 app.use(bodyParser.json());
@@ -23,50 +29,23 @@ app.post('/receberloc', async (req, res) => {
     console.log(`Localização recebida: x=${data.bairro.x}, y=${data.bairro.y}, z=${data.bairro.z}`);
     console.log(`Jogador: ${data.jogador}`);
 
-    // Pegar os cinco primeiros dígitos de x e y
-    const x = Math.round(data.bairro.x);
-    const y = Math.round(data.bairro.y);
-    const xFirstFiveDigits = String(x).substring(0, 5);
-    const yFirstFiveDigits = String(y).substring(0, 5);
-
-    // Atualizar as coordenadas para os cinco primeiros dígitos de x e y
-    data.bairro.x = xFirstFiveDigits;
-    data.bairro.y = yFirstFiveDigits;
-
     try {
-        // Conectar ao banco de dados
         const client = await pool.connect();
-
-        // Executar a consulta SQL para inserir as coordenadas no banco de dados
-        const result = await client.query(
-            'INSERT INTO coordenadas (x, y) VALUES ($1, $2)',
-            [data.bairro.x, data.bairro.y]
-        );
-
-        // Liberar o cliente da conexão
+        const queryText = 'INSERT INTO coordenadas (x, y, z, jogador) VALUES ($1, $2, $3, $4)';
+        const values = [data.bairro.x, data.bairro.y, data.bairro.z, data.jogador];
+        const result = await client.query(queryText, values);
         client.release();
-
-        console.log('Coordenadas inseridas com sucesso no banco de dados');
-
-        // Emitir evento para atualizar as coordenadas no cliente WebSocket
+        console.log('Coordenadas salvas no banco de dados.');
         io.emit('atualizarLocalizacao', { x: data.bairro.x, y: data.bairro.y });
-
-        res.end('Mensagem recebida pelo servidor');
+        res.status(200).end('Coordenadas recebidas e salvas com sucesso.');
     } catch (err) {
-        console.error('Erro ao inserir coordenadas no banco de dados', err);
-        res.status(500).end('Erro ao inserir coordenadas no banco de dados');
+        console.error('Erro ao salvar coordenadas no banco de dados:', err);
+        res.status(500).end('Erro interno do servidor.');
     }
-});
-
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + 'index.html');
 });
 
 io.on('connection', (socket) => {
     console.log('Cliente WebSocket conectado');
-    socket.on('atualizarLocalizacao', (data) => {
-        socket.broadcast.emit('atualizarLocalizacao', data);
-    });
     socket.on('disconnect', () => {
         console.log('Cliente WebSocket desconectado');
     });
