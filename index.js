4a60d5f3e6da7b9df991abaed1faddc71ef743a4
GabrielSam23 <143095210+GabrielSam23@users.noreply.github.com>
2024-05-15 23:41:39 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -2,15 +2,18 @@ const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
 const bodyParser = require('body-parser');
+const { Pool } = require('pg');
+require('dotenv').config(); // Para carregar as variáveis de ambiente do arquivo .env
 
 const app = express();
 const server = http.createServer(app);
 const io = socketIo(server);
+const pool = new Pool(); // Usará as variáveis de ambiente para configuração
 
 app.use(express.static('.'));
 app.use(bodyParser.json());
 
-app.post('/receberloc', (req, res) => {
+app.post('/receberloc', async (req, res) => {
     const data = req.body;
     if (!data.bairro) {
         console.log('Erro: bairro não definido');
@@ -30,11 +33,30 @@ app.post('/receberloc', (req, res) => {
     data.bairro.x = xFirstFiveDigits;
     data.bairro.y = yFirstFiveDigits;
 
-    io.emit('atualizarLocalizacao', { x: data.bairro.x, y: data.bairro.y });
+    try {
+        // Conectar ao banco de dados
+        const client = await pool.connect();
 
-    res.end('Mensagem recebida pelo servidor');
+        // Executar a consulta SQL para inserir as coordenadas no banco de dados
+        const result = await client.query(
+            'INSERT INTO coordenadas (x, y) VALUES ($1, $2)',
+            [data.bairro.x, data.bairro.y]
+        );
+
+        // Liberar o cliente da conexão
+        client.release();
+
+        console.log('Coordenadas inseridas com sucesso no banco de dados');
+
+        // Emitir evento para atualizar as coordenadas no cliente WebSocket
+        io.emit('atualizarLocalizacao', { x: data.bairro.x, y: data.bairro.y });
+
+        res.end('Mensagem recebida pelo servidor');
+    } catch (err) {
+        console.error('Erro ao inserir coordenadas no banco de dados', err);
+        res.status(500).end('Erro ao inserir coordenadas no banco de dados');
+    }
 });
-
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + 'index.html');
@@ -50,7 +72,7 @@ io.on('connection', (socket) => {
     });
 });
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 server.listen(port, () => {
     console.log(`Servidor está rodando em http://localhost:${port}`);
 });
