4a98f6b6f9fea0f9889bd2bd421d659d0acc8705
GabrielSam23 <143095210+GabrielSam23@users.noreply.github.com>
2024-07-20 16:32:03 -0300

    Update index.js

--- index.js
+++ index.js
@@ -74,6 +74,7 @@ app.post('/receberloc', async (req, res) => {
         `;
         const values = [data.bairro.x, data.bairro.y, data.bairro.z, data.jogador];
         await pool.query(upsertQuery, values);
+        io.emit('atualizarLocalizacao');
     } catch (error) {
         console.error('Erro ao inserir ou atualizar coordenadas no banco de dados:', error);
         res.status(500).end('Erro ao inserir ou atualizar coordenadas no banco de dados');
@@ -98,6 +99,7 @@ app.delete('/receberloc', async (req, res) => {
         const updateQuery = 'UPDATE coordenadas SET gps_ativo = false WHERE jogador = $1';
         const values = [data.jogador];
         await pool.query(updateQuery, values);
+        io.emit('atualizarLocalizacao');
     } catch (error) {
         console.error('Erro ao desativar GPS no banco de dados:', error);
         res.status(500).end('Erro ao desativar GPS no banco de dados');
@@ -105,6 +107,13 @@ app.delete('/receberloc', async (req, res) => {
     }
 
     res.status(200).end('GPS desativado com sucesso');
+});
+
+io.on('connection', (socket) => {
+    console.log('Novo cliente conectado');
+    socket.on('disconnect', () => {
+        console.log('Cliente desconectado');
+    });
 });
 
 const port = process.env.PORT || 3000;
